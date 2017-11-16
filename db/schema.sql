856b67a258d2dfc3852960ea246b92e248d07bad
Joseph <joekimga@gmail.com>
2017-11-16 18:10:19 -0500

    WIP: public folder

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,2 @@
 ### Schema
-CREATE DATABASE burgers_db;
-USE burgers_db;
-
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
-	devoured BOOLEAN DEFAULT false,
-	date a TIMESTAMP
-	PRIMARY KEY (id)
-);
 
